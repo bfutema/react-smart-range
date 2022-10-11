69bbd6786eb5a146eb4906692ca67da102cffe4d
Bruno Futema <bruno.futema@outlook.com>
2022-10-11 04:08:57 -0300

    fix(storybook): :wrench: Add storybook configuration (#1)

---
+++ src/components/SmartRange/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 200px;
+  height: 32px;
+
+  border: 1px solid red;
+  border-radius: 4px;
+
+  color: #f7f7f7;
+  line-height: 14px;
+
+  padding: 2px 8px;
+`;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { SmartRange } from './SmartRange';
